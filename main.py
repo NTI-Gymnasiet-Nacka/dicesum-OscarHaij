0088c2548fd8e30aa9021a569de5a003f58557b7
Oscar Haij <oscar.haij@elev.ga.ntig.se>
2024-08-21 10:28:49 +0200

    Oscar Haij

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-# Dice sum probability calculator
-# Författare: 
-# Datum:
 
 def main():
     user_input = input().split(" ")
@@ -10,10 +7,12 @@ def main():
     else:
         if int(user_input[0]) >= int(user_input[1]):
             for i in range(int(user_input[0]) - int(user_input[1])+1):
-                print(i + int(user_input[0])+1)
+                print(i + int(user_input[1])+1)
         else:
             for i in range(int(user_input[1]) - int(user_input[0])+1):
-                print(i + int(user_input[0])+1)
+             print(i + int(user_input[0])+1)
+
+
 
 if __name__ == "__main__":
     main()
