911dc08e571648c3e44c9362c75e6b1f79fc4ba8
Oscar Haij <oscar.haij@elev.ga.ntig.se>
2024-08-21 10:20:01 +0200

    Oscar Haij Tärning.prog

--- main.py
+++ main.py
@@ -4,6 +4,15 @@
 
 def main():
     user_input = input().split(" ")
+    if int(user_input[0]) == int(user_input[1]):
+        print(int(user_input[0])+1)
+    else:
+        if user_input[0] >= user_input[1]:
+            for i in range(int(user_input[0]) - int(user_input[1])+1):
+                print(i + int(user_input[0])+1)
+        else:
+            for i in range(int(user_input[1]) - int(user_input[0])+1):
+                print(i + int(user_input[0])+1)
 
 if __name__ == "__main__":
     main()
